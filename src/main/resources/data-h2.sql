3c83a40dab94fa7c338c54ea55b6da5c6f84ee0e
ishrn <igorshr@mail.ru>
2022-03-30 18:46:47 +0300

    Add bootstrap data for customer
    Close #4

---
+++ src/main/resources/data-h2.sql
@@ -0,0 +1,5 @@
+INSERT INTO "PUBLIC"."CUSTOMER" VALUES
+(1, 'josiah_faulkner@mail.com', 'Josiah', 'Faulkner'),
+(2, 'maxim_ochoa@mail.com', 'Maxim', 'Ochoa'),
+(3, 'angus_fowler@mail.com', 'Angus', 'Fowler'),
+(4, 'euan_york@mail.com', 'Euan', 'York');
