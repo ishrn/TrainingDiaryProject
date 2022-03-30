dd80b3f18d40719baf3dbcdf6ff391007d204446
ishrn <igorshr@mail.ru>
2022-03-30 13:49:45 +0300

    Initial commit

---
+++ src/test/java/com/training/diary/DiaryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.training.diary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiaryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
