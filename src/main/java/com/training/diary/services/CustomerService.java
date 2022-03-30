efe4d454a929a7aedfcbca551d8cf1d2a293b006
ishrn <igorshr@mail.ru>
2022-03-30 19:34:17 +0300

    Add customer service
    Close #5

---
+++ src/main/java/com/training/diary/services/CustomerService.java
@@ -0,0 +1,23 @@
+package com.training.diary.services;
+
+import com.training.diary.domains.Customer;
+
+import java.util.Optional;
+
+public interface CustomerService {
+    Customer save(Customer entity);
+
+    Optional<Customer> findById(Long id);
+
+    boolean existsById(Long id);
+
+    Iterable<Customer> findAll();
+
+    long count();
+
+    void deleteById(Long id);
+
+    void delete(Customer entity);
+
+    void deleteAll();
+}
---
+++ src/main/java/com/training/diary/services/CustomerServiceImpl.java
@@ -0,0 +1,57 @@
+package com.training.diary.services;
+
+import com.training.diary.domains.Customer;
+import com.training.diary.repositories.CustomerRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class CustomerServiceImpl implements CustomerService {
+
+    private final CustomerRepository customerRepository;
+
+    public CustomerServiceImpl(CustomerRepository customerRepository) {
+        this.customerRepository = customerRepository;
+    }
+
+    @Override
+    public Customer save(Customer entity) {
+        return customerRepository.save(entity);
+    }
+
+    @Override
+    public Optional<Customer> findById(Long id) {
+        return customerRepository.findById(id);
+    }
+
+    @Override
+    public boolean existsById(Long id) {
+        return customerRepository.existsById(id);
+    }
+
+    @Override
+    public Iterable<Customer> findAll() {
+        return customerRepository.findAll();
+    }
+
+    @Override
+    public long count() {
+        return customerRepository.count();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        customerRepository.deleteById(id);
+    }
+
+    @Override
+    public void delete(Customer entity) {
+        customerRepository.delete(entity);
+    }
+
+    @Override
+    public void deleteAll() {
+        customerRepository.deleteAll();
+    }
+}
