82eaa18386ba5addd8bd4184fd789782c45c0c40
ishrn <igorshr@mail.ru>
2022-04-01 15:12:04 +0300

    Set customer no args constructor to private
    Close #9

--- src/main/java/com/training/diary/models/Customer.java
+++ src/main/java/com/training/diary/models/Customer.java
@@ -10,7 +10,7 @@ import javax.persistence.Id;
 @Builder
 @ToString
 @EqualsAndHashCode
-@NoArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 public class Customer {
 
